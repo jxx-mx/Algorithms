b3d2e08936f59687e8239574f076dd2edbdfab8c
jxx-mx <fierce.unarmed_0l@icloud.com>
2023-06-10 17:07:06 +0900

    합

--- 합(8393)/index.js
+++ 합(8393)/index.js
@@ -2,14 +2,20 @@
 
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
-let data = Number(input[0]);
+let n = Number(input[0]);
 
+// for문 이용
 let result = 0;
-
 function reducer() {
-  for (let i = 0; i <= data; i++) {
+  for (let i = 0; i <= n; i++) {
     result += i;
   }
   console.log(result);
 }
 reducer();
+
+// 등차수열 합 공식 이용
+function foo() {
+  console.log((n * (n + 1)) / 2);
+}
+foo();
