c6fe47a3b0d910126d48ec5136ca654f6e5e0b54
jxx-mx <fierce.unarmed_0l@icloud.com>
2023-06-15 16:37:55 +0900

    배열 최솟값, 최댓값

--- 배열 최솟값 최댓값(10818)/index.js
+++ 배열 최솟값 최댓값(10818)/index.js
@@ -1,25 +1,19 @@
 // 문제 출처: https://www.acmicpc.net/problem/10818
 
-/**
- * web에서 제공되는 api를 사용한 경우가 반복문을 사용한 경우보다 빠른 성능을 보이는 반면 메모리를 조금 더 사용함.
- * useWebApi: 92088KB, 436ms
- * useLoop: 88340KB, 484ms
- */
-
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
 let length = Number(input[0]);
 let data = input[1].split(" ").map(Number);
 
-function useWebApi() {
+function useMath() {
   let maxNum = Math.max(...data);
   let minNum = Math.min(...data);
 
-  console.log("hi", minNum, maxNum);
+  console.log("Math", minNum, maxNum);
 }
 
-useWebApi();
+useMath();
 
 function useLoop() {
   let maxValue = -1000001;
@@ -29,7 +23,15 @@ function useLoop() {
     if (minValue > data[i]) minValue = data[i];
     if (maxValue < data[i]) maxValue = data[i];
   }
-  console.log(minValue, maxValue);
+  console.log("loop", minValue, maxValue);
 }
 
 useLoop();
+
+function useReduce() {
+  let minValue = data.reduce((a, b) => Math.min(a, b));
+  let maxValue = data.reduce((a, b) => Math.max(a, b));
+  console.log("reduce", minValue, maxValue);
+}
+
+useReduce();
