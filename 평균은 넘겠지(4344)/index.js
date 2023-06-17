555dbd349d43b4c58033f422d7f365178b0623c6
jxx-mx <fierce.unarmed_0l@icloud.com>
2023-06-17 16:38:38 +0900

    평균은 넘겠지

--- 평균은 넘겠지(4344)/index.js
+++ 평균은 넘겠지(4344)/index.js
@@ -3,9 +3,10 @@
 let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().split("\n");
 
-let testCases = Number(input[0]);
+// 1안
+let testCases1 = Number(input[0]);
 
-for (let t = 1; t <= testCases; t++) {
+for (let t = 1; t <= testCases1; t++) {
   let data = input[t].split(" ").map(Number);
   let length = data[0];
   let sum = 0;
@@ -22,4 +23,23 @@ for (let t = 1; t <= testCases; t++) {
   }
 
   console.log(`${((count / length) * 100).toFixed(3)}%`);
+}
+
+// 2안
+function aboveAverageRatio(scores) {
+  const n = scores[0];
+  const averages = scores.slice(1).reduce((sum, score) => sum + score, 0) / n;
+  const aboveAverageCount = scores
+    .slice(1)
+    .filter((score) => score > averages).length;
+  const aboveAveragePercentage = (aboveAverageCount / n) * 100;
+  return aboveAveragePercentage.toFixed(3) + "%";
+}
+
+const testCaseCount = Number(input[0]);
+const testCases2 = input.slice(1);
+
+for (let i = 0; i < testCaseCount; i++) {
+  const scores = testCases2[i].split(" ").map(Number);
+  console.log(aboveAverageRatio(scores));
 }
